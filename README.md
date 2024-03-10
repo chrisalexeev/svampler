bcdfd0269084c7ec77eb9fce959c4782f0091b71
Christopher Alexeev <chrisalexeev@gmail.com>
2024-03-10 13:08:08 -0400

    forward

--- README.md
+++ README.md
@@ -4,9 +4,11 @@ Svampler is an drum machine/sampler written in Svelte and typescript. [Try the d
 
 ## Goal
 
-I am building this to learn the Web Audio API, Svelte, Rust, and WASM. At the moment there is no Rust or WASM in the main branch.
+This started as a way to learn the Web Audio API, Svelte, Rust, and WASM (at the moment there is no Rust or WASM in the main branch).
 
-The long-term goal of this app is to be an in-browser production studio, tailored to my personal workflow, that can handle additional DSP via Rust+WASM.
+Currently, I am in the process of redesigning this as a full application, _not just a toy_. The `sampler.ts` implementation will be broken out to be a full, modular, audio routing engine, of which the drum machine will be a single module. Upcoming improvements include synthesizers, effect racks, a mix bus, arranger, and more.
+
+The long-term goal of this app is to be an in-browser production studio, tailored to my personal workflow, that can handle additional DSP via Rust+WASM to create 🔥 _~certified bangers~_ 🔥.
 
 ## Setup
 
--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,28 @@
 
 <main>
   {#if !init}
+    <div id="forward">
+      <h1>Svampler 🥁</h1>
+      <p>
+        This started as a way to learn the Web Audio API, Svelte, Rust, and WASM
+        (at the moment there is no Rust or WASM in the main branch).
+      </p>
+      <p>
+        Currently, I am in the process of redesigning this as a full
+        application, <em>not just a toy</em>. The <code>sampler.ts</code> implementation will be
+        broken out to be a full, modular, audio routing engine, of which the
+        drum machine will be a single module. Upcoming improvements include
+        synthesizers, effect racks, a mix bus, arranger, and more.
+      </p>
+      <p>
+        The long-term goal of this app is to be an in-browser production studio,
+        tailored to my personal workflow, that can handle additional DSP via
+        Rust+WASM to create
+      </p>
+      <p class="bangers">
+        🔥 <em>~certified bangers~</em> 🔥.
+      </p>
+    </div>
     <button
       on:click={() => {
         init = true;
@@ -18,6 +40,7 @@
 <style>
   main {
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
     height: 100%;
@@ -31,5 +54,23 @@
   /* Hide scrollbar for Chrome, Safari and Opera */
   main::-webkit-scrollbar {
     display: none;
+  }
+
+  #forward {
+    max-width: 50%;
+  }
+
+  #forward h1 {
+    font-size: 3rem;
+    text-align: center;
+  }
+
+  #forward .bangers {
+    text-align: center;
+  }
+
+  code {
+    background-color: hsl(0, 0%, 20%);
+    border-radius: 0.25rem;
   }
 </style>
